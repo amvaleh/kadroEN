2b1d1ab92dfc482985b4bdd3751863dc9e559ae8
amirmahdi valeh <amvaleh@gmail.com>
2023-05-15 13:32:08 +0330

    add blog link and add sms alert for admin

--- app/models/call_request.rb
+++ app/models/call_request.rb
@@ -6,10 +6,7 @@ class CallRequest < ApplicationRecord
   after_create :alert_admin
 
   def alert_admin
-    sms_message = "ثبت درخواست مشاوره جدید"
-    SmsWorker.perform_async(sms_message, "09206152175")
+    sms_message = "shootempire new CR."
     SmsWorker.perform_async(sms_message, "09353954916")
-    SmsWorker.perform_async(sms_message, "09123807488")
-    SmsWorker.perform_async(sms_message, "09024608026")
   end
 end
